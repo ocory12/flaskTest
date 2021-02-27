43e35cb24039a3a320cbde167c0f70cc87c972cb
ocory12 <ojcory@gmail.com>
2021-02-27 19:52:18 +0000

    Addition of file upload and removal of hello world

--- app.py
+++ app.py
@@ -5,5 +5,13 @@ app = Flask(__name__)
 def home_page():
     return render_template('index.html')
 
+@app.route('/', methods=['POST'])
+def upload_file():
+    uploaded_file = request.files['file']
+    if uploaded_file.filename != '':
+        uploaded_file.save(uploaded_file.filename)
+    return redirect(url_for('index'))
+
+
 if __name__ == '__main__':
     app.run()
